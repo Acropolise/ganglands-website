3f94fda2bec42d412808029f5a3ad3186ee86b3e
Acropolise <osherbe3@gmail.com>
2025-07-13 20:47:16 +0300

    paths change

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/steam-game-showcase-site">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
